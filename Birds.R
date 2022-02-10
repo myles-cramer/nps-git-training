1fd9f6b9605e75038c00fc9061b58ad3ad4f6a63
myles-cramer <myles.j.cramer@gmail.cpom>
2022-02-10 13:13:44 -0500

    Enable Tidyverse

--- Birds.R
+++ Birds.R
@@ -2,4 +2,8 @@
 birds <- read.csv("https://raw.githubusercontent.com/KateMMiller/IMD_R_Training_Advanced/main/data/birds.csv")
 
 # Preview dataset
-head(birds)
+head(birds)head(birds)
+
+# Load packages
+library(dplyr)
+library(readr)
